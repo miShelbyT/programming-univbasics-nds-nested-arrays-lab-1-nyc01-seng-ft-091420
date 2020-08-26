2322d14e933f30c24decef3d8da2972e77829113
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-26 20:16:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,16 +25,10 @@ end
 
 
 
-
-
-
-
-
-  ]# Build an array that contains both of the above arrays
+  # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
-end
 
 def sorted_matrix
   # Using Array literal syntax only, build another nested array that
