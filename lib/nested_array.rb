f6583ad31d00d7a31732c4f5b055baf6cc425932
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-26 20:21:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix
     CONVENTIONAL_PRODUCE,
     ORGANIC_PRODUCE
   ]
-  return produce
+produce
 end
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
