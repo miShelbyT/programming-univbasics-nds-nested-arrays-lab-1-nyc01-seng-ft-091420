7ae182cbcb2b99d51dcd97faf13da36aa9688d7e
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-26 20:18:05 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,9 +22,6 @@ def assembled_matrix
     ORGANIC_PRODUCE,
   produce
 end
-
-
-
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
